e6f5c639bca05d133ce8b0dc1247e897ac2ef3a5
Shah Fahad <shah.fahad@tenpearls.com>
2022-03-04 02:31:29 +0500

    Post tracker location

--- src/components/Tracker.vue
+++ src/components/Tracker.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-      <v-row class="text-center">
+    <v-row align="center" justify="center">
       <v-col cols="12">
         <v-img
           :src="require('../assets/logo.svg')"
@@ -10,35 +10,121 @@
         />
       </v-col>
 
-      <v-col class="mb-4">
-      <h1>TRACKER COMPONENT</h1>
-      <pre>`{{ users }}</pre>
+      <v-col class="mb-4" cols="6">
+        <h1>TRACKER COMPONENT</h1>
+        <v-card class="form-card" elevation="2">
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="30"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
+
+            <v-btn
+              color="success"
+              class="mr-4"
+              v-if="!isTracking"
+              @click="onTracking"
+            >
+              Start tracking
+            </v-btn>
+
+            <v-btn
+              color="error"
+              class="mr-4"
+              v-if="isTracking"
+              @click="onStopTracking"
+            >
+              Stop tracking
+            </v-btn>
+
+            <pre>{{ coordinates }}</pre>
+          </v-form>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 import axios from 'axios';
 
 @Component
 export default class Tracker extends Vue {
+  private coordinates = {
+    latitude: '',
+    longitude: '',
+  };
+  private locationWatchId: number = 0;
+  private intervalEvent:number = 0;
+  private isTracking = false;
+  valid = false;
+  name = '';
+  nameRules = [
+    (v: string) => !!v || 'Name is required',
+    (v: string) =>
+      (v && v.length <= 30) || 'Name must be less than 10 characters',
+  ];
 
-  private users: Array<any> = [];
-  
-  mounted() {
-    console.log('TRACKER MOUNTED : ');
-    this.getUsers();
+  onTracking() {
+    if (!this.name.trim()) {
+      (this.$refs.form as Vue & { validate: () => boolean }).validate()
+      return;
+    }
+    this.isTracking = true;
+    this.getLocation();
+    this.startLocationTimer();
+    console.log('START TRACKING DEVICE');
   }
 
-  async getUsers() {
-    console.log('FETCH USERS ');
-    const result = await axios.get('https://tracker-api-m11.herokuapp.com/users');
-    // const result = await axios.get('http://localhost:3000/users');
-    console.log('USERS ', result.data);
-    this.users = result.data;
+  onStopTracking() {
+    console.log('STOP TRACKING DEVICE');
+    this.isTracking = false;
+    navigator.geolocation.clearWatch(this.locationWatchId);
+    clearInterval(this.intervalEvent);
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      this.locationWatchId = navigator.geolocation.watchPosition(
+        this.showPosition
+      );
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }
+
+  async showPosition(position: any) {
+    this.coordinates.latitude = position.coords.latitude;
+    this.coordinates.longitude = position.coords.longitude;
+  }
+
+  startLocationTimer() {
+    this.intervalEvent = setInterval(this.postTrackerLocation, 5 * 1000);
+  }
+
+  async postTrackerLocation() {
+    console.log(
+      'UPDATING TRACKER LOCATION : ',
+      this.name,
+      this.coordinates.latitude,
+      this.coordinates.longitude
+    );
+    if (this.name && this.coordinates.latitude) {
+      const payload = {
+        name: this.name,
+        latitude: this.coordinates.latitude,
+        longitude: this.coordinates.longitude,
+      };
+      const result = await axios.put(
+        'https://tracker-api-m11.herokuapp.com/tracker',
+        payload
+      );
+    }
   }
 }
 </script>
@@ -58,5 +144,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.form-card {
+  padding: 20px;
 }
 </style>
