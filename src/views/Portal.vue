5d885d550580c69a8a0fabb4bd838742ec459825
Shah Fahad <shah.fahad@tenpearls.com>
2022-03-08 00:37:35 +0500

    Add portal configuration

---
+++ src/views/Portal.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>PORTAL</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+export default class Home extends Vue {}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,32 @@
+const { argv } = require('yargs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const app = argv.app || 'default';
+
+const appConfig = {
+  main: {
+    entryApp: './src/main.ts',
+    title: 'Tracker App',
+    outputDir: './dist',
+  },
+  portal: {
+    entryApp: './src/portal-main.ts',
+    title: 'Tracker Portal',
+    outputDir: './dist-portal',
+  },
+};
+appConfig.default = appConfig.main;
+
 module.exports = {
   lintOnSave: false,
 
-  transpileDependencies: ["vuetify"],
+  transpileDependencies: ['vuetify'],
+  configureWebpack: (config) => {
+    console.log('ENTRY APP : ', app);
+    config.entry.app = appConfig[app].entryApp;
+    let htmlWebpackPlugin = config.plugins.find(
+      (plugin) => plugin instanceof HtmlWebpackPlugin
+    );
+    htmlWebpackPlugin.options.title = appConfig[app].title;
+  },
 };
